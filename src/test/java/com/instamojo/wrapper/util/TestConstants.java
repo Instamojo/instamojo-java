85b53b75bc2d84c3dd42d85e2d57094eec34a910
Abeer <abeer.u@instamojo.com>
2018-12-26 18:15:51 +0530

    Added credentials for testing account created on test.instamojo.com
    - Created a new account on test.instamojo.com which will be used for api testing.
    
    Signed-off-by: Abeer <abeer.u@instamojo.com>

--- src/test/java/com/instamojo/wrapper/util/TestConstants.java
+++ src/test/java/com/instamojo/wrapper/util/TestConstants.java
@@ -7,6 +7,12 @@ public class TestConstants {
 
     private TestConstants(){}
 
+    public static final String USERNAME = "im_testing";
+    public static final String PASSWORD = "test@123";
+
+    public static final String CLIENT_ID = "test_dcmBxsYtYjnxacApKdP8owW2w5DsoGLY5xB";
+    public static final String CLIENT_SECRET = "test_zRYyx77j1W8qe6Z1RANp19pLgIEkqB95QPd1NAzY0Xa8BfGFymN4iKOZO6kBwHM4aNwFg6NhRYLzo8BWzF2OwrEHsEQVXMWpx1V3r5UYNHDeKcMMmBT7unuwAXo";
+
     /**
      * The constant TEST_CLIENT_ID.
      */
